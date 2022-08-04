e6ba1c0e2b0f2b49f3c667798d358f08529c0c6f
chojangg <s2115@e-mirim.hs.kr>
2022-08-04 15:45:06 +0900

    Jangbaguni

---
+++ Jangbaguni/app/src/main/java/com/example/jangbaguni/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.jangbaguni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.start_btn).setOnClickListener {
+
+        }
+    }
+
+
+}
